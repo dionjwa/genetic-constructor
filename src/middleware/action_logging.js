bc898954052c88ffbda7c038a7e5c85072479681
Maxwell Bates <maxbates@gmail.com>
2017-04-18 11:07:12 -0700

    lint fix

--- src/middleware/action_logging.js
+++ src/middleware/action_logging.js
@@ -2,10 +2,16 @@ const loggingMiddleware = store => next => (action) => {
   const result = next(action);
 
   if (process.env.DEBUG_REDUX === 'true') {
-    console.group && console.group(action.type);
+    if (console.group) {
+      console.group(action.type);
+    }
+    
     console.log('dispatching', action);
     console.log('next state', store.getState());
-    console.groupEnd && console.groupEnd(action.type);
+    
+    if (console.groupEnd) {
+      console.groupEnd(action.type);
+    }
   }
 
   return result;
