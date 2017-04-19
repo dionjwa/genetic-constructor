85fbdc6a7a0ae3540552c79dd2a2c84d92c2caa5
Justin McCandless <justinjmccandless@gmail.com>
2017-04-19 10:23:27 -0700

    E2E spec for dragging in initial block to big drop zone

--- test-e2e/tests/drag-to-create-construct.js
+++ test-e2e/tests/drag-to-create-construct.js
@@ -1,6 +1,8 @@
 var homepageRegister = require('../fixtures/homepage-register');
-var testProject = require('../fixtures/testproject');
+var dragFromTo = require('../fixtures/dragfromto');
+var openInventoryPanel = require('../fixtures/open-inventory-panel');
 var size = require('../fixtures/size');
+
 module.exports = {
   'Test that dropping on the project canvas creates a new construct.' : function (browser) {
     size(browser);
@@ -10,9 +12,16 @@ module.exports = {
     browser
       // wait for inventory and inspector to be present
       .waitForElementPresent('.SidePanel.Inventory', 5000, 'Expected Inventory Groups')
-      .waitForElementPresent('.SidePanel.Inspector', 5000, 'Expected Inspector');
+      .waitForElementPresent('.SidePanel.Inspector', 5000, 'Expected Inspector')
+      .click('.construct-viewer .inline-toolbar img[data-id="Delete Construct"]')
+      .waitForElementNotPresent('.construct-viewer', 500, 'Clicked to delete only construct, and now the canvas is empty.')
+      .assert.countelements('.inter-construct-drop-target', 1, 'When canvas is empty, there is exactly one drop target.');
 
-    testProject(browser);
+    openInventoryPanel(browser, 'Sketch');
+    dragFromTo(browser, '.InventoryGroupRole .sbol-tile:nth-of-type(1) .RoleSvg', 27, 27, '.inter-construct-drop-target', 600, 400);
+
+    browser
+      .waitForElementPresent('.construct-viewer', 500, 'After dragging in a block, a construct was created.');
 
     // generate image for testing.
     browser
